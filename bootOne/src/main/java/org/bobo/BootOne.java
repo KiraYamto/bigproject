3072712a06ceb5d83a83cf7a350281226e8eb251
0027006602 <tian.xubo@iwhalecloud.com>
2020-08-25 17:17:55 +0800

    Springboot+Mybatis整合 1.完整的接口調用，Controller 2.完整的接口+數據庫查詢 3.結合日誌輸出

--- bootOne/src/main/java/org/bobo/BootOne.java
+++ bootOne/src/main/java/org/bobo/BootOne.java
@@ -1,12 +1,17 @@
 package org.bobo;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("org.bobo.mybatis.mapper")
 public class BootOne {
     public static void main(String[] args) {
         System.out.println("------BootOne Start----------");
--- bootOne/src/main/java/org/bobo/controller/TestController.java
+++ bootOne/src/main/java/org/bobo/controller/TestController.java
@@ -1,14 +1,19 @@
 package org.bobo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
 public class TestController {
 
-    @RequestMapping(value = "/test", method = RequestMethod.POST)
-    public String testRequest() {
+    @RequestMapping(value = {"/one"}, method = RequestMethod.POST)
+   public String testOne(@RequestBody String requestBody, HttpServletRequest request, HttpServletResponse response){
         System.out.println("testRequest");
         return "ok";
     }
