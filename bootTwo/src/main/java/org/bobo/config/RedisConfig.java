050bcadda2bdeeb5c4811f3771f1ddd2e7e0554c
0027006602 <tian.xubo@iwhalecloud.com>
2020-10-13 09:40:09 +0800

    1.增加springcache+redis，redis采用集群cluster部署，cache有多个注解，例子只有@CachePut(value ="cache-one")，其中cache-one为redis-config类中指定配置， 可以配置多个cache的不同系列化方式以及过期时间 2.yml配置增加 spring:   redis:     database: 0     timeout: 60s     cluster:       nodes: 172.16.22.55:7001,172.16.22.55:7002,172.16.22.55:7003,172.16.22.55:7004,172.16.22.55:7005,172.16.22.55:7006     jedis:       pool:         max-active: 8         max-idle: 8         max-wait: -1         min-idle: 0     password: 123456  3.pom依赖增加：          <!-- springredis -->          <dependency>              <groupId>org.springframework.boot</groupId>              <artifactId>spring-boot-starter-data-redis</artifactId>          </dependency>

---
+++ bootTwo/src/main/java/org/bobo/config/RedisConfig.java
@@ -0,0 +1,104 @@
+package org.bobo.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    private Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+
+    /**
+     * 注解@Cache key生成规则
+     */
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append("-");
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append("-");
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    /**
+     * 注解@Cache的管理器，设置过期时间的单位是秒
+     *
+     * @param redisConnectionFactory
+     * @return
+     * @Description:
+     */
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        //解决查询缓存转换异常的问题
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        // 配置序列化（解决乱码的问题）
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+                .disableCachingNullValues();
+
+
+
+        // 设置一个初始化的缓存空间set集合
+        Set<String> cacheNames =  new HashSet<>();
+        cacheNames.add("cache-one");
+        cacheNames.add("cache-two");
+
+        // 对每个缓存空间应用不同的配置
+        Map<String, RedisCacheConfiguration> configMap = new HashMap<>();
+        configMap.put("cache-one", config.entryTtl(Duration.ofMinutes(2)));
+        configMap.put("cache-two", config.entryTtl(Duration.ofMinutes(1)));
+        RedisCacheManager cacheManager = RedisCacheManager.builder(redisConnectionFactory)     // 使用自定义的缓存配置初始化一个cacheManager
+                .initialCacheNames(cacheNames)  // 注意这两句的调用顺序，一定要先调用该方法设置初始化的缓存名，再初始化相关的配置
+                .withInitialCacheConfigurations(configMap)
+                .build();
+        return cacheManager;
+    }
+
+
+
+
+
+
+}
--- bootTwo/src/main/java/org/bobo/service/impl/NsNstTemplateServiceImpl.java
+++ bootTwo/src/main/java/org/bobo/service/impl/NsNstTemplateServiceImpl.java
@@ -7,6 +7,7 @@ import org.bobo.mybatis.query.NsNstTemplateQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +19,8 @@ public class NsNstTemplateServiceImpl  {
 
     @Autowired
     private NsNstTemplateMapper nsNstTemplateMapper;
+
+    @CachePut(value ="cache-one")
     public List<NsNstTemplateDto> selectNsNstTemplateList(String nstCode, String nstName) {
         NsNstTemplateQuery query = new NsNstTemplateQuery(nstCode,nstName);
         List<NsNstTemplateDto> nstTemplateList = nsNstTemplateMapper.selectList(query);
